c75b654db49caa985ac82eea6b5506fce4d1cf72
WisdomTheKreator <chimawisdom153@gmail.com>
2026-04-25 15:30:50 +0100

    Removed the esbuild and re-installed the node dependencies, tested the build locally and it's working

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    cssMinify: 'esbuild', //instead of lightingcss
+  css: {
+    transformer: 'postcss',   // stops lightningcss from running at all
   },
 })
